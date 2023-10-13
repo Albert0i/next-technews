d2cf7319d6d92ad1b4a3932ff52f18f8b20f241d
Alberto Iong <albert0i@hotmail.com>
2023-10-13 15:23:18 +0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import CategoriesList from "@/components/CategoriesList";
+import Post from "@/components/Post";
+import { postsData } from '@../../data'
+
+export default function Home() {
+  return (<div>
+            <CategoriesList />
+
+            { postsData && postsData.length > 0 ? 
+              postsData.map((post) => ( <Post key={post.id} post={post} /> ) ) : 
+              <div className='py-6'>No posts to display</div> } 
+        </div>)
+}
