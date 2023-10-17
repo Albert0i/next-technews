449a84f267f0d925578f197cc1c91c7640d46d6b
Alberto Iong <albert0i@hotmail.com>
2023-10-17 11:10:46 +0800

    add prisma

--- src/lib/prismadb.js
+++ src/lib/prismadb.js
@@ -11,3 +11,10 @@ const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+/*
+   Best practice for instantiating PrismaClient with Next.js
+   https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
+   @auth/prisma-adapter
+   https://authjs.dev/reference/adapter/prisma
+*/
