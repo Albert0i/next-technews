4919ed9b2766ccb87b63630cfe7a1a4565a72c46
Alberto Iong <albert0i@hotmail.com>
2023-10-25 12:48:48 +0800

    part 15 pretty much

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,6 +1,21 @@
 'use client'
 
 const DeleteButton = ({id}) => {  
+  const deleteImage = async(publicId) => {
+    try {
+      const res = await fetch('/api/removeImage', {
+        method: 'DELETE', 
+        headers: { "Content-Type":  "application/json" }, 
+        body: JSON.stringify({ publicId })
+      })
+      if (!res.ok) {
+        console.log('res=', res)
+      }
+    } catch (error) {
+      console.log(error)
+    }  
+  }
+  
   const handleDelete = async () => {
     const confirmed = confirm('Are you sure to delete this post?')
     if (confirmed) {
@@ -11,6 +26,10 @@ const DeleteButton = ({id}) => {
         })
         if (res.ok) {
           console.log('Post deleted')
+          const post = await res.json()
+          console.log('post=', post)
+          const { publicId } = post          
+          await deleteImage(publicId)
         }
       } catch (error) {
         console.log(error)
